60d8f085085c2726058bbd8732e07d7692ec0de5
Jeremy <jeremy24wu@gmail.com>
2025-08-10 21:16:00 +1000

    add BrowserTabManager.cs for managing method calls

---
+++ BrowserTabs/BrowserTabManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Automation;
+using System.Linq;
+
+namespace BrowserTabs
+{
+    /// <summary>
+    /// Retrieve, activate, and close tabs in Chromium-based browsers.
+    /// </summary>
+    public class BrowserTabManager
+    {
+        private static readonly string[] ChromiumProcessNames = new[]
+        {
+            "msedge", "chrome", "brave", "vivaldi", "opera", "chromium"
+        };
+    }
+}
